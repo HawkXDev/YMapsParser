a6bbe5e578aa42c279cf5ca29f333646019a28df
sokolserg <sergsokolkin@gmail.com>
2023-07-19 19:43:20 +0300

    connected slf4j-log4j12

--- src/main/java/com/sokol/LaunchChrome.java
+++ src/main/java/com/sokol/LaunchChrome.java
@@ -1,21 +1,29 @@
 package com.sokol;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.sokol.util.PropertiesUtil.getProperty;
 
 public class LaunchChrome {
 
+    private static final Logger log = LoggerFactory.getLogger(LaunchChrome.class);
+
     public static void main(String[] args) {
         String url = getProperty("url");
 
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        try (ChromeDriverService service = new ChromeDriverService.Builder()
+                .withLogOutput(System.out)
+                .build();) {
 
-        driver.get(url);
-        System.out.println(driver.getTitle());
+            ChromeDriver driver = new ChromeDriver(service);
 
-        driver.quit();
+            driver.get(url);
+            log.info("Title: {}", driver.getTitle());
+
+            driver.quit();
+        }
     }
 }
