9ae69bfca35f13b72d88ad3691b0fb1c0ac879fd
sokolserg <sergsokolkin@gmail.com>
2023-07-19 17:45:10 +0300

    use ClassLoader to get resources

--- src/main/java/com/sokol/util/PropertiesUtil.java
+++ src/main/java/com/sokol/util/PropertiesUtil.java
@@ -1,5 +1,7 @@
 package com.sokol.util;
 
+import com.sokol.LaunchChrome;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -12,7 +14,9 @@ public final class PropertiesUtil {
 
     public static String getProperty(String key) {
         Properties properties = new Properties();
-        try (InputStream input = Files.newInputStream(Paths.get("src/main/resources/prop.properties"))) {
+
+        try (InputStream input = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("prop.properties")) {
             properties.load(input);
             return properties.getProperty("url");
         } catch (IOException e) {
