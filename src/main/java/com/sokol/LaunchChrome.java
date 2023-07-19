8e616966f3030eb928957ef1da0c21aa3df41b6f
sokolserg <sergsokolkin@gmail.com>
2023-07-19 20:57:06 +0300

    scrollAndGetAllSearchSnippets

--- src/main/java/com/sokol/LaunchChrome.java
+++ src/main/java/com/sokol/LaunchChrome.java
@@ -1,9 +1,15 @@
 package com.sokol;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.sokol.util.PropertiesUtil.getProperty;
 
@@ -14,16 +20,62 @@ public class LaunchChrome {
     public static void main(String[] args) {
         String url = getProperty("url");
 
-        try (ChromeDriverService service = new ChromeDriverService.Builder()
-                .withLogOutput(System.out)
-                .build();) {
+        ChromeDriverService service = null;
+        ChromeDriver driver = null;
 
-            ChromeDriver driver = new ChromeDriver(service);
+        try {
+            service = new ChromeDriverService.Builder()
+                    .withLogOutput(System.out)
+                    .build();
+
+            driver = new ChromeDriver(service);
 
             driver.get(url);
             log.info("Title: {}", driver.getTitle());
 
-            driver.quit();
+            WebElement scrollContent = driver.findElement(By.className("scroll__container"));
+
+            List<WebElement> searchSnippets = scrollAndGetAllSearchSnippets(driver, scrollContent);
+            log.info("Snippets: {}", searchSnippets.size());
+
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+            if (service != null) {
+                service.stop();
+            }
         }
+    }
+
+    private static void scrollToEnd(ChromeDriver driver, WebElement element) {
+        ((JavascriptExecutor) driver)
+                .executeScript("arguments[0].scrollTo(0, arguments[0].scrollHeight)", element);
+    }
+
+    private static List<WebElement> scrollAndGetAllSearchSnippets(ChromeDriver driver, WebElement scrollContent) {
+        List<WebElement> searchSnippets = new ArrayList<>();
+
+        while (true) {
+            scrollToEnd(driver, scrollContent);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+
+            List<WebElement> currentSearchSnippets = scrollContent.findElements(By.className("search-snippet-view"));
+            log.info("currentSearchSnippets: {}", currentSearchSnippets.size());
+
+            if (currentSearchSnippets.size() == searchSnippets.size()) {
+                break;
+            }
+
+            searchSnippets = new ArrayList<>(currentSearchSnippets);
+        }
+
+        return searchSnippets;
     }
 }
