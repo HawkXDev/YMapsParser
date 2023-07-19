38e7efa9724a2ce76f433f43ebbe7a893c7a6559
sokolserg <sergsokolkin@gmail.com>
2023-07-19 17:40:26 +0300

    get url from resources

--- src/main/java/com/sokol/LaunchChrome.java
+++ src/main/java/com/sokol/LaunchChrome.java
@@ -3,15 +3,23 @@ package com.sokol;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static com.sokol.util.PropertiesUtil.getProperty;
+
 public class LaunchChrome {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        String url = getProperty("url");
+        System.out.println(url);
 
-        driver.get("https://www.google.com");
-        System.out.println(driver.getTitle());
 
-        driver.quit();
+//        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.get("https://www.google.com");
+//        System.out.println(driver.getTitle());
+//
+//        driver.quit();
+
+
     }
 }
---
+++ src/main/java/com/sokol/util/PropertiesUtil.java
@@ -0,0 +1,23 @@
+package com.sokol.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public final class PropertiesUtil {
+    private PropertiesUtil() {
+    }
+
+    public static String getProperty(String key) {
+        Properties properties = new Properties();
+        try (InputStream input = Files.newInputStream(Paths.get("src/main/resources/prop.properties"))) {
+            properties.load(input);
+            return properties.getProperty("url");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
